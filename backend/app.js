88a91108c06c763840fe753a10853eb3209ebaaa
0xmarWan7A <m5500volly@gmail.com>
2024-11-01 14:14:06 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL, // Allow only this origin
+  origin: "https://elections-ivory.vercel.app", // Allow only this origin
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
 
