1d1a82a6e4e4dc6e174c6f1cb88c1367039b4fe3
0xmarWan7A <m5500volly@gmail.com>
2024-11-03 02:34:41 +0200

    Add files via upload

--- backend/routes/vote.routes.js
+++ backend/routes/vote.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   vote,
   listUsersWithVotingInfo,
+  clearUserVote,
 } from "../controller/vote.controller.js";
 import { adminAuth, candidateRoute } from "../middleware/auth.middleware.js";
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/", candidateRoute, vote);
 router.get("/", candidateRoute, adminAuth, listUsersWithVotingInfo);
+router.delete("/:id", candidateRoute, adminAuth, clearUserVote);
 
 export default router;
